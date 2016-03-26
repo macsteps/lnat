dba348575920be083b3e193e77119b48afc577b2
macsteps <macsteps@gmail.com>
2016-03-26 08:53:32 -0700

    minor tweaks to header style. add validations for user email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+ validates :email, presence: true, length: { maximum: 255 },
+                   format: { with: VALID_EMAIL_REGEX },
+                   uniqueness: { case_sensitive: false }
+
 end
