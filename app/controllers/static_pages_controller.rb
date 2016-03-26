4faa07525af0e91583434e39402ca281b3a09a11
macsteps <macsteps@gmail.com>
2016-03-25 22:43:45 -0700

    add settings page & route, confirmable & lockable to user model, and customize header & nav.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: [:settings]
+
   def home
+  end
+
+  def settings
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root                  'static_pages#home'
+  root                       'static_pages#home'
+  get 'settings'          => 'static_pages#settings'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
