64f9daf73f41ccbc1c1d72edb4c8883960a1dd93
macsteps <macsteps@gmail.com>
2016-04-01 06:46:27 -0700

    remove extra /blog dir from url.

--- app/controllers/markdown_blog_controller.rb
+++ app/controllers/markdown_blog_controller.rb
@@ -3,17 +3,17 @@ class MarkdownBlogController < ApplicationController
   attr_reader :content
 
   def index
-    posts = Dir.glob("#{Rails.root}/public/blog/*/*/*.md")
+    posts = Dir.glob("#{Rails.root}/public/*/*/*.md")
     @content = Array.new
     posts.each do |post|
       discard, slug = post.split(/lnat_app\//)
       slug_fields = []
-      slug_fields = slug.match(/(\w+?\/\w+?\/)(\w+?)\/(\d{4})\/(\S+?\.md)/)
+      slug_fields = slug.match(/(\w+?\/)(\w+?)\/(\d{4})\/(\S+?\.md)/)
       category = slug_fields[2]
       year = slug_fields[3]
       file_name = slug_fields[4]
       title = file_name.split(/-/).map(&:capitalize).join(' ').gsub(/\.md$/, '')
-      slug_final = "public/blog/#{category}/#{year}/#{file_name}"
+      slug_final = "public/#{category}/#{year}/#{file_name}"
       slug_final.html_safe
       logger.debug "slug final: #{slug_final}"
       @content << [title, slug_final.html_safe]
