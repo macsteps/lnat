a9a87c6c6ba3b2b0b959b7da391d535c7f30ff30
macsteps <macsteps@gmail.com>
2016-04-19 10:46:47 -0700

    add item and product validations.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
   has_many :products, through: :cart
+  validates_associated :products
+  validates :quantity,  presence: true,
+                        numericality: { only_integer: true,
+                                        greater_than_or_equal_to: 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   belongs_to :item
+  validates :name, presence: true
+  validates :quantity,  presence: true,
+                        numericality: { only_integer: true,
+                                        greater_than_or_equal_to: 0 }
+  validates :price,     presence: true,
+                        numericality: { only_integer: true,
+                                        greater_than_or_equal_to: 0 }
 end
